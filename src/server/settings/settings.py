b0a7f05e6fe4901e736373c04902712badacb7a7
yeonsookim40282628 <gjb2628@gmail.com>
2020-04-04 01:15:20 +0900

    define 파일

---
+++ src/server/settings/settings.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+# define
+HOST_ADDR = '127.0.0.1'  # localhost address
+SERVER_PORT = 8000  # 서버 포트
+DEBUG = False  # 디버그모드
+MODE = os.environ.get('MODE')
+
+# connect db
+USER = 'postgres'  # username
+PASSWOLRD = 'root'  # postgresql pw
+DB_PORT = '5432'  # postgresql port
+NAME = 'test'  # db name
+POSTGRESQL = f'postgresql://{USER}:{PASSWOLRD}@{HOST_ADDR}:{DB_PORT}/{NAME}'  # postgresql uri
+
+# select operation mode
+if MODE == 'TEST' or sys.argv[0].endswith('test'):  # use only pytest
+    HOST_ADDR = '127.0.0.1'
+    DEBUG = False
+elif MODE == 'DEV':  # mode - development
+    HOST_ADDR = '127.0.0.1'
+    DEBUG = True
+elif MODE == 'RUN':  # mode - release
+    HOST_ADDR = '0.0.0.0'
+    DEBUG = False
+else:  # select not permission mode
+    raise KeyError
