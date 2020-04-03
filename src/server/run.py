33e5892c8fac744094e4e010c35cc9c95ddd9b51
yeonsookim40282628 <gjb2628@gmail.com>
2020-04-03 21:27:46 +0900

    wsgi 서버 실행파일

---
+++ src/server/run.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from gevent.pywsgi import WSGIServer
+
+from settings.settings import HOST_ADDR, SERVER_PORT, MODE
+from settings.wsgi import create_wsgi
+
+app = create_wsgi()
+
+# manager
+db = SQLAlchemy(app)  # connect db
+if __name__ == '__main__':
+    if MODE == 'DEV':
+        app.run(host=HOST_ADDR, port=SERVER_PORT)
+    elif MODE == 'RUN':
+        app = WSGIServer((HOST_ADDR, SERVER_PORT), app, log=None)
+        app.serve_forever()
