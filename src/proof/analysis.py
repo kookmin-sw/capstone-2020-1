c0e30aeecc33db4061a0fb3b5b270c44fd57a9d2
HyeongJunPark <asurava@naver.com>
2020-04-05 17:18:20 +0900

    채팅량을 단위시간(분)으로 쪼개고 단위시간 내에서 채팅이 가장 많은 지점(초)을 리턴하는 함수 추가

---
+++ src/proof/analysis.py
@@ -0,0 +1,156 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def double_digit(num):
+    if num < 10:
+        return '0' + str(num)
+    else:
+        return str(num)
+
+
+def print_point(second):
+    minute = int(second / 60)
+    if minute >= 60:
+        hour = int(minute / 60)
+        minute %= 60
+    else:
+        hour = int(minute / 60)
+    second = int(second % 60)
+    print(double_digit(hour) + ':' + double_digit(minute) + ':' + double_digit(second))
+
+
+def analyze1(data, comment=None):  # 초당 채팅 수 계산
+    second = []
+    if comment is None:
+        # 채팅 기록에서 모든 comment의 시간 추출
+        for i in range(len(data)):
+            second.append(int(data[i][0]))
+    else:
+        # 채팅 기록에서 특정 comment가 포함된 채팅 시간 추출
+        for i in range(len(data)):
+            if comment in data[i][2]:
+                second.append(int(data[i][0]))
+
+    count = []
+    for i in range(int(data[len(data) - 1][0]) + 1):
+        count.append(0)
+
+    for x in second:
+        count[x] += 1
+
+    average = np.mean(np.array(count))
+
+    chk = []
+    for x in range(len(count)):
+        chk.append(average)
+
+    # 오름차순으로 정렬한 후 10 번째로 많은 채팅 수를 구함
+    max10th = sorted(count)[-100]
+
+    point = []
+    print("편집점:")
+    for i in range(len(count)):
+        if count[i] >= max10th:
+            point.append(i)
+            print_point(i)
+
+    """
+    plt.bar(range(len(count)), count)  # 채팅 수
+    # plt.twinx()
+    # plt.plot(range(len(count)), chk, 'r--')  # 평균
+    plt.xlabel("second")
+    plt.ylabel("chat / second")
+    plt.show()
+    """
+    return point
+
+
+def analyze2(data):  # 초당 채팅 참가자 수 계산
+    second = []
+
+    for i in range(len(data)):
+        second.append(int(data[i][0]))
+
+    count_s = []
+    for i in range(int(data[len(data) - 1][0]) + 1):
+        count_s.append(0)
+
+    for x in second:
+        count_s[x] += 1
+
+    base = 0
+    user = []
+    count_u = []
+    temp = []
+    for i in range(len(count_s)):
+        x = count_s[i]
+        for j in range(base, base + x):
+            temp.append(data[j][1])
+        temp = list(set(temp))
+        count_u.append(len(temp))
+        user.extend(temp)
+        user = list(set(user))
+        base += x
+        temp = []
+    total_user = len(user)
+    del user
+    print("총 채팅 참여자 수:", total_user)
+
+    average = np.mean(np.array(count_u))
+
+    chk = []
+    for x in range(len(count_u)):
+        chk.append(average)
+
+    # 오름차순으로 정렬한 후 10 번째로 많은 채팅 참여자 수를 구함
+    max10th = sorted(count_u)[-10]
+
+    point = []
+    print("편집점:")
+    for i in range(len(count_u)):
+        if count_u[i] >= max10th:
+            point.append(i)
+            print_point(i)
+    return point
+    plt.bar(range(len(count_u)), count_u)  # 채팅 참여자 수
+    # plt.twinx()
+    # plt.plot(range(len(count)), chk, 'r--')  # 평균
+    plt.xlabel("second")
+    plt.ylabel("chat / second")
+    plt.show()
+
+
+def analyze1_minute(data, comment=None):#분단위로 쪼개고 해당 단위시간(분) 내에 가장 채팅이 많은 지점(초)을 리턴
+    second = []
+    if comment is None:
+        # 채팅 기록에서 모든 comment의 시간 추출
+        for i in range(len(data)):
+            second.append(int(data[i][0]))
+    else:
+        # 채팅 기록에서 특정 comment가 포함된 채팅 시간 추출
+        for i in range(len(data)):
+            if comment in data[i][2]:
+                second.append(int(data[i][0]))
+
+    count = []
+    for i in range(int(data[len(data) - 1][0]) + 1):
+        count.append(0)
+
+    for x in second:
+        count[x] += 1
+    
+    minute = []
+    cut = 0#60초 단위로 쪼개기 위한 변수
+    while True:
+        try:
+            arr = count[cut:cut+60]
+            minute.append( (arr.index(max(arr)),max(arr)) )#(시간(초),채팅량) 튜플로 저장
+            cut += 60
+        except:
+            break
+    
+    for i in range(len(minute)):
+        print_point( 60*i + minute[i][0] )
+    
+    return minute
