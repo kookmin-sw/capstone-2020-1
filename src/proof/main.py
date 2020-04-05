5c35877cbde7dcaff8d225989e69b4fa4dc14ce6
NobleWhale <22051174+Noble-Whale@users.noreply.github.com>
2020-04-05 20:26:02 +0900

    소리, 채팅 분석코드 수정 및 추가

--- src/proof/main.py
+++ src/proof/main.py
@@ -1,5 +1,7 @@
 import download
 import volume_extract as sound
+import analysis
+import sys
 
 """
 [참고 사이트]
@@ -12,19 +14,31 @@ https://github.com/mcpcseal/TwitchChatAnalyzer
 https://nbviewer.jupyter.org/github/tark1998/twitch-searching-highlight-through-comments-counting/tree/master/
 https://steemit.com/kr-dev/@steemonen1/twitch
 """
-# # videoID로 초당 채팅 수 시각화
-# afreeca_videoID = "54055792"
-# download.afreeca(afreeca_videoID)
 
-# twitch_videoID = "562644795"
-# download.twitch(twitch_videoID)
+
+# # videoID로 초당 채팅 수 시각화
+afreeca_videoID = "54518239"
+afreeca_chat = download.afreeca(afreeca_videoID)
+afreeca_point1 = analysis.analyze1_minute(afreeca_chat)
+
+"""
+twitch_videoID = "568292071"
+twitch_chat = download.twitch(twitch_videoID)
+twitch_point1 = analysis.analyze1_minute(twitch_chat)
+"""
 
 # youtube_videoID = "VUzT3OEPtQk"
 # download.youtube(youtube_videoID)
 
+
 # 영상에서 초당 소리의 크기 시각화
-volume = sound.sound_extract("full.mp4")
-print(sum(volume)/len(volume))
-volume = sound.sound_extract("highlight.mp4")
-print(sum(volume)/len(volume))
+file_name = sys.argv[1]
+volume = sound.sound_extract(file_name)
+point = analysis.analyze1_sound(volume)
+
+#print(sum(volume)/len(volume))
+
+
+#volume = sound.sound_extract("highlight.mp4")
+#print(sum(volume)/len(volume))
 
