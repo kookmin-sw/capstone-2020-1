3e8e085ecd3a9779d865874837e33a4ca30ca6e7
JinseonHeo <wlstjs2478@naver.com>
2020-04-07 23:36:48 +0900

    feat: Video, Audio, Chatlog를 다운 받는 기능 보충
    1. URL을 입력하면 AfreecaTV, Twitch, Youtube 중 어떤 플랫폼의 주소인지 판별하고 정제하여 사용한다.
    2. Video, Audio는 가장 낮은 옵션으로 다운받는다.
    3. 다운받은 Video, Audio, Chatlog는 각각의 폴더에 위치한다.
    
    Related to: #18

--- src/download/audio.py
+++ src/download/audio.py
@@ -1,88 +1,46 @@
-import youtube_dl
-import ffmpeg
 import os
-# 사용전 pip3 install youtube-dl
+import re
+import youtube_dl
+# pip3 install youtube-dl
 
-def download_audio_twitch(video_list):
-    for video_url in video_list:
 
-        if not os.path.exists("./download"):
-            os.makedirs("./download")
-        download_path = os.path.join('./download', '%(title)s' + '.mp3')
+def download(platform, videoID, url):
+    dw_opts = {'format': 'worstaudio/worst', 'extractaudio': True, 'audioformat': "mp3", 'outtmpl': "audio/" + platform + '_' + videoID + ".mp3"}
+    try:
+        with youtube_dl.YoutubeDL(dw_opts) as ydl:
+            ydl.download([url])
+    except Exception as e:
+        print('error', e)
+    print('Finish!')
 
-        dw_opts = {'format' : 'Audio_Only',
-                   'resolution' : 'audio only',
-                   'audioformat': 'mp3',
-                   'outtmpl' : download_path
-                   }
-                   #}
 
-        t = 0
-        while t == 0:
-            try:
-                with youtube_dl.YoutubeDL(dw_opts) as ydl:
-                    t = 1
-                    ydl.download([video_url])
-            except Exception as e:
-                t = 0
-                print('error', e)
+if __name__ == '__main__':
+    url = input("stream url : ")
 
-def download_audio_afreeca(video_list):
-    for video_url in video_list:
-
-        if not os.path.exists("./download"):
-            os.makedirs("./download")
-        download_path = os.path.join('./download', '%(title)s' + '.mp3')
-
-        dw_opts = {'extension': 'worst',
-                   'resolution': 'audio_only',
-                   'audioformat' : 'mp3',
-                   'outtmpl': download_path}
-
-        t = 0
-        while t == 0:
-            try:
-                with youtube_dl.YoutubeDL(dw_opts) as ydl:
-                    t = 1
-                    ydl.download([video_url])
-            except Exception as e:
-                t = 0
-                print('error', e)
-
-def download_audio_youtube(video_list):
-    for video_url in video_list:
-
-        if not os.path.exists("./download"):
-            os.makedirs("./download")
-        download_path = os.path.join('./download', '%(title)s' + '.mp3')
-
-        dw_opts = {'format' : '249',
-                   'resolution' : 'audio only',
-                   'audioformat' : 'mp3',
-                   'outtmpl': download_path}
-
-        try:
-            with youtube_dl.YoutubeDL(dw_opts) as ydl:
-                ydl.download([video_url])
-        except Exception as e:
-            print('error', e)
-
-def main():
-    url_list_a = []
-    url_list_y = []
-    url_list_t = []
-    while True:
-        url = input("영상주소를 입력하세요(그만 입력하려면 'q'를 눌러주세요):")
-        if url == 'q':
-            break
+    if "afree" in url:
+        platform = "AfreecaTV"
+        if "afreecatv" in url:
+            url = re.search(r"http://vod.afreecatv.com/PLAYER/STATION/[0-9]+", url).group()
+        videoID = url.split('/')
+        videoID = videoID[-1]
+    elif "twitch" in url:
+        platform = "Twitch"
+        url = re.search(r"https://www.twitch.tv/videos/[0-9]+", url).group()
+        videoID = url.split('/')
+        videoID = videoID[-1]
+    elif "youtu" in url:
+        platform = "Youtube"
+        if 'youtube' in url:
+            url = re.search(r"https://www.youtube.com/watch\?v=[a-zA-Z0-_]+", url).group()
+            videoID = url.split('=')
         else:
-            if 'afreecatv' in url:
-                url_list_a.append(url)
-            elif 'youtube' in url:
-                url_list_y.append(url)
-            else:
-                url_list_t.append(url)
+            url = re.search(r"https://youtu.be/[a-zA-Z0-_]+", url).group()
+            videoID = url.split('/')
+        videoID = videoID[-1]
 
-    download_audio_afreeca(url_list_a)
-    download_audio_twitch(url_list_t)
-    download_audio_youtube(url_list_y)
+    if not os.path.exists("./audio"):
+        os.makedirs("./audio")
+    if platform+"_"+videoID+".mp3" in os.listdir("./audio"):
+        print ('This audio file has already been requested.')
+    else:
+        download(platform, videoID, url)
