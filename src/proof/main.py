8bae97306dc3f58f4ae01691e244bf198c2550da
JinseonHeo <wlstjs2478@naver.com>
2020-04-08 11:10:26 +0900

    feat: 비디오, 오디오, 채팅 추출기능 추가

--- src/proof/main.py
+++ src/proof/main.py
@@ -1,44 +1,81 @@
-import download
+import os
+import re
+import video
+import audio
+import chatlog
 import volume_extract as sound
 import analysis
-import sys
+
+
+url = input("stream url : ")
+platform = ""
+videoID = ""
+
+if "afree" in url:
+    # AfreecaTV 주소형식
+    # http://vod.afreecatv.com/PLAYER/STATION/videoID
+    # http://v.afree.ca/ST/videoID
+    platform = "AfreecaTV"
+    if "afreecatv" in url:
+        url = re.search(r"http://vod.afreecatv.com/PLAYER/STATION/[0-9]+", url).group()
+    else:
+        url = re.search(r"http://v.afree.ca/ST/[0-9]+", url).group()
+    videoID = url.split('/')
+    videoID = videoID[-1]
+elif "twitch" in url:
+    # Twitch 주소형식
+    # https://www.twitch.tv/videos/videoID
+    platform = "Twitch"
+    url = re.search(r"https://www.twitch.tv/videos/[0-9]+", url).group()
+    videoID = url.split('/')
+    videoID = videoID[-1]
+elif "youtu" in url:
+    # Youtube 주소형식
+    # https://www.youtube.com/watch?v=videoID
+    # https://youtu.be/videoID
+    platform = "Youtube"
+    if 'youtube' in url:
+        url = re.search(r"https://www.youtube.com/watch\?v=[a-zA-Z0-9_-]+", url).group()
+        videoID = url.split('=')
+    else:
+        url = re.search(r"https://youtu.be/[a-zA-Z0-9_-]+", url).group()
+        videoID = url.split('/')
+    videoID = videoID[-1]
 
 """
-[참고 사이트]
-- 아프리카
-https://www.afreecatv.com/player/Player.xml
-https://cafe.naver.com/tsoul7/74753
-https://github.com/minkyoJang/WD_AI-BigData
-- 트위치
-https://github.com/mcpcseal/TwitchChatAnalyzer
-https://nbviewer.jupyter.org/github/tark1998/twitch-searching-highlight-through-comments-counting/tree/master/
-https://steemit.com/kr-dev/@steemonen1/twitch
+if not os.path.exists("./video"):
+    os.makedirs("./video")
+if platform+"_"+videoID+".mp4" in os.listdir("./video"):
+    print('This video file has already been requested.')
+    volume = sound.sound_extract(platform, videoID, "video")
+else:
+    video.download(platform, videoID, url)
+    volume = sound.sound_extract(platform, videoID, "video")
 """
 
+if not os.path.exists("./audio"):
+    os.makedirs("./audio")
+if platform + "_" + videoID + ".mp3" in os.listdir("./audio"):
+    print('This audio file has already been requested.')
+    volume = sound.sound_extract(platform, videoID)
+    print("< audio point >")
+    audio_point = analysis.analyze1_sound(volume)
+else:
+    audio.download(platform, videoID, url)
+    volume = sound.sound_extract(platform, videoID)
+    print("< audio point >")
+    audio_point = analysis.analyze1_sound(volume)
 
-# # videoID로 초당 채팅 수 시각화
-afreeca_videoID = "54518239"
-afreeca_chat = download.afreeca(afreeca_videoID)
-afreeca_point1 = analysis.analyze1_minute(afreeca_chat)
+if not os.path.exists("./chatlog"):
+    os.makedirs("./chatlog")
+if platform + "_" + videoID + ".txt" in os.listdir("./chatlog"):
+    print('This chatlog file has already been requested.')
+    chat_data = chatlog.download(platform, videoID)
+    print("< chatlog point >")
+    chat_point = analysis.analyze1_minute(chat_data)
+else:
+    chat_data = chatlog.download(platform, videoID)
+    print("< chatlog point >")
+    chat_point = analysis.analyze1_minute(chat_data)
 
-"""
-twitch_videoID = "568292071"
-twitch_chat = download.twitch(twitch_videoID)
-twitch_point1 = analysis.analyze1_minute(twitch_chat)
-"""
-
-# youtube_videoID = "VUzT3OEPtQk"
-# download.youtube(youtube_videoID)
-
-
-# 영상에서 초당 소리의 크기 시각화
-file_name = sys.argv[1]
-volume = sound.sound_extract(file_name)
-point = analysis.analyze1_sound(volume)
-
-#print(sum(volume)/len(volume))
-
-
-#volume = sound.sound_extract("highlight.mp4")
-#print(sum(volume)/len(volume))
 
