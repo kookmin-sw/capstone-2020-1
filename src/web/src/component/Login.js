d502b8c2a5059b80f811c0791ca2302af28122e1
SanghunShin <tlstkdgns95@gmail.com>
2020-04-15 23:08:52 +0900

    코드 스타일 정리

--- src/web/src/component/Login.js
+++ src/web/src/component/Login.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -22,16 +22,15 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Login =()=> {
+const Login = () => {
   const classes = useStyles();
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-
         <Typography component="h1" variant="h5">
-          If you want using YOBA service <br/>
+          If you want using YOBA service <br />
           Plz, Login
         </Typography>
         <form className={classes.form} noValidate>
@@ -70,6 +69,6 @@ const Login =()=> {
       </div>
     </Container>
   );
-}
+};
 
 export default Login;
