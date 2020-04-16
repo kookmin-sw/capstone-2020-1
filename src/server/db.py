5b9909260069f5384b3d1f32116be34ad540237a
yeonsookim40282628 <gjb2628@gmail.com>
2020-04-17 01:37:23 +0900

    Base추가, 오타수정

--- src/server/db.py
+++ src/server/db.py
@@ -1,10 +1,12 @@
 from settings.settings import POSTGRESQL
 from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(POSTGRESQL, connect_args={'connect_timeout': 10})
 Session = sessionmaker(bind=engine)
 metadata = MetaData()
+Base = declarative_base(metadata=metadata)
 
 
 class Database:
