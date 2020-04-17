332640d3d3f0bc84c12c5e453fd01a4f8ea3eacf
SanghunShin <tlstkdgns95@gmail.com>
2020-04-18 00:42:17 +0900

    잘못된 변수명 바꾸기, 하이라이트 테이블 component(mocking)

--- src/web/src/component/Highlight.js
+++ src/web/src/component/Highlight.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles({
   table: {
@@ -14,16 +14,16 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs) {
-  return { name, calories, fat, carbs };
+function createData(number, start, end, kind) {
+  return { number, start, end, kind };
 }
 
 const rows = [
-  createData('Highlight 1', '18:00', '19:00', 'chat'),
-  createData('Highlight 2', '28:00', '29:00', 'sound'),
-  createData('Highlight 3', '38:00', '39:00', 'chat'),
-  createData('Highlight 4', '48:00', '49:00', 'chat'),
-  createData('Highlight 5', '58:00', '59:00', 'sound'),
+  createData("Highlight 1", "18:00", "19:00", "chat"),
+  createData("Highlight 2", "28:00", "29:00", "sound"),
+  createData("Highlight 3", "38:00", "39:00", "chat"),
+  createData("Highlight 4", "48:00", "49:00", "chat"),
+  createData("Highlight 5", "58:00", "59:00", "sound"),
 ];
 
 export default function Highlight() {
@@ -31,7 +31,7 @@ export default function Highlight() {
 
   return (
     <TableContainer component={Paper}>
-       <h3 className="mt-5">Highlight Point</h3>
+      <h3 className="mt-5">Highlight Point</h3>
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -43,13 +43,13 @@ export default function Highlight() {
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <TableRow key={row.name}>
+            <TableRow key={row.number}>
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.number}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.start}</TableCell>
+              <TableCell align="right">{row.end}</TableCell>
+              <TableCell align="right">{row.kind}</TableCell>
             </TableRow>
           ))}
         </TableBody>
