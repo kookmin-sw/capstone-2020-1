cd10685ebd035bfae496e4c5dbe4ae687586c2b5
HyeongJunPark <asurava@naver.com>
2020-05-01 00:21:13 +0900

    필요 없는 주석 제거 및 리팩토링

--- src/server/api/SNDnormalize.py
+++ src/server/api/SNDnormalize.py
@@ -15,8 +15,7 @@ app = Blueprint('SNDnormalize', __name__, url_prefix='/api')
 @app.route('/SNDnormalize', methods=['GET'])
 @api
 def get_sound_normalize(data, db):
-    
-    req_list = ['platform', 'videoid','url']
+    req_list = ['platform', 'videoid', 'url']
     for i in req_list:  # 필수 요소 들어있는지 검사
         if i not in data:
             raise BadRequest
@@ -24,17 +23,12 @@ def get_sound_normalize(data, db):
     platform = data['platform']
     videoid = data['videoid']
     url = data['url']
-    #url = re.search(r"https://www.twitch.tv/videos/[0-9]+", url).group()
-    #videoID = url.split('/')
-    #videoID = videoID[-1]
-    
+
     download(platform, videoid, url)
     audio = AudioFileClip(f"audio/{platform}_{videoid}_NA.mp3")
-    volumesPerMinute = sound_extract(platform,videoid)
-    
+    volumesPerMinute = sound_extract(platform, videoid)
+
     audio_arr, avg = local_normalize(audio, volumesPerMinute)
-    #print("1")
-    #arr = audio_arr.to_soundarray()
-    #print("2")
-    
-    return jsonify({"average":avg})
+    # arr = audio_arr.to_soundarray()
+
+    return jsonify({"average": avg})
