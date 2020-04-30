54c72eff849e11ef2e2fdf7adc866c6f211851d6
NobleWhale <22051174+Noble-Whale@users.noreply.github.com>
2020-05-01 00:25:06 +0900

    audio test 코드 수정

--- src/server/tests/test_sound.py
+++ src/server/tests/test_sound.py
@@ -2,10 +2,12 @@ from flask import json
 
 
 def test_get_test(client):
-    data = {"platform":"Twitch"}
+    data = {"platform": "Twitch"}
     res = client.get('/api/SNDnormalize', query_string=data)
     assert res.status_code == 400
 
-    data = {"platform":"Twitch", "videoid":562644795,"url":"https://www.twitch.tv/videos/562644795https://www.twitch.tv/videos/562644795"}
+    data = {"platform": "Twitch",
+            "videoid": 562644795,
+            "url": "https://www.twitch.tv/videos/562644795https://www.twitch.tv/videos/562644795"}
     res = client.get('/api/SNDnormalize', query_string=data)
     assert res.status_code == 200
