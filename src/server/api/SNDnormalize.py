a9d0e1f59b2e5d96dd22d4bd39d2c723be1926d2
HyeongJunPark <asurava@naver.com>
2020-05-09 15:57:37 +0900

    sound normalize 호출시 url 유효성 검사 추가

--- src/server/api/SNDnormalize.py
+++ src/server/api/SNDnormalize.py
@@ -3,11 +3,13 @@ import sys
 sys.path.append('../')
 
 from flask import Blueprint, jsonify
+from werkzeug.exceptions import BadRequest, NotAcceptable
+
+from server.api.Non_url import *
 from settings.utils import api
 from download.audio import *
 from analyze.volume_extract import *
 from moviepy.editor import *
-from werkzeug.exceptions import BadRequest
 
 app = Blueprint('SNDnormalize', __name__, url_prefix='/api')
 
@@ -23,11 +25,26 @@ def get_sound_normalize(data, db):
     url = data['url']
 
     platform, videoid = extractInfoFromURL(url)
-    download(platform, videoid, url)
-    audio = AudioFileClip(f"audio/{platform}_{videoid}_NA.mp3")
-    volumesPerMinute = sound_extract(platform, videoid)
 
-    audio_arr, avg = local_normalize(audio, volumesPerMinute)
-    # arr = audio_arr.to_soundarray()
+    if platform == "Twitch":
+        check = non_url_twitch(videoid)
+        isValid = True if check == "recorded" else False
+    elif platform == "Youtube":
+        check = non_url_youtube(videoid)
+        isValid = True if check == "OK" else False
+    elif platform == "AfreecaTV":
+        check = non_url_afreeca(videoid)
+        isValid = True if check > 2 else False
 
-    return jsonify({"average": avg})
+    if isValid == True:
+        download(platform, videoid, url)
+        audio = AudioFileClip(f"audio/{platform}_{videoid}_NA.mp3")
+        volumesPerMinute = sound_extract(platform, videoid)
+
+        audio_arr, avg = local_normalize(audio, volumesPerMinute)
+
+        # arr = audio_arr.to_soundarray()
+
+        return jsonify({"average": avg})
+    else:
+        raise NotAcceptable # 유효하지 않은 URL
