f02e156b94329f197c1b1b510061d144f0c3631f
SongSeongJae <tjdkskgnal61@gmail.com>
2020-05-11 21:10:02 +0800

    feat: Modify input param to url

--- src/server/api/predict.py
+++ src/server/api/predict.py
@@ -7,6 +7,8 @@ from settings.utils import api
 from chatsentiment.pos_neg_spm import *
 from werkzeug.exceptions import BadRequest
 from urllib.parse import urlencode
+from ana_url import split_url
+from download.chatlog import *
 import math
 
 app = Blueprint('predict', __name__, url_prefix='/api')
@@ -14,9 +16,18 @@ app = Blueprint('predict', __name__, url_prefix='/api')
 
 @app.route('/predict', methods=['GET'])
 @api
-def get_predict(data, db):
-    second = request.args.getlist("second")
-    content = request.args.getlist("content")
+def get_predict(url):
+    platform, videoID = split_url(url)
+    download(platform, videoID)
+    with open('../../download/chatlog/{}_{}.txt'.format(platform, videoID)) as f:
+        content = f.read().split('\n')
+    second=[]
+    content=[]
+    for i in content:
+        splited_chat = i.split('\t')
+        second.append(splited_chat[0])
+        content.append(splited_chat[2])
+
     if len(second) < 1 or len(content) < 1:
         raise BadRequest
     
