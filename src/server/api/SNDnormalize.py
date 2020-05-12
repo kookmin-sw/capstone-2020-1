a93064d79053356d550d5e18f8642ddd2bb72fc0
example <example@example.com>
2020-05-12 14:29:45 +0900

    url 유효성 검사 수정 및 테스트코드 수정

--- src/server/api/SNDnormalize.py
+++ src/server/api/SNDnormalize.py
@@ -49,18 +49,12 @@ def get_sound_normalize(data, db):
 
     platform, videoid = extractInfoFromURL(url)
 
-    """
     if platform == "Twitch":
-        check = non_url_twitch(videoid)
-        isValid = True if check == "recorded" else False
+        isValid = non_url_twitch(videoid)
     elif platform == "Youtube":
-        check = non_url_youtube(videoid)
-        isValid = True if check == "OK" else False
+        isValid = non_url_youtube(videoid)
     elif platform == "AfreecaTV":
-        check = non_url_afreeca(videoid)
-        isValid = True if check > 2 else False
-    """
-    isValid = True
+        isValid = non_url_afreeca(videoid)
 
     if isValid == True:
         download(platform, videoid, url)
--- src/server/tests/test_sound.py
+++ src/server/tests/test_sound.py
@@ -13,3 +13,7 @@ def test_get_normalize(client):
     data = {"url": "https://youtu.be/rOFRZ-zBAbs"} # 이미 들어왔던 데이터
     res = client.get('/api/SNDnormalize', query_string=data)
     assert res.status_code == 409
+
+    data = {"url": "https://www.twitch.tv/videos/562644795"}  # 유효하지 않은 url
+    res = client.get('/api/SNDnormalize', query_string=data)
+    assert res.status_code == 406
