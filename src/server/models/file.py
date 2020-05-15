63af1befc95039dd562d4c9c2cdd58239427fe74
yeonsookim40282628 <gjb2628@gmail.com>
2020-05-16 02:55:43 +0900

    image url로 리턴하게 변경

--- src/server/models/file.py
+++ src/server/models/file.py
@@ -10,6 +10,7 @@ class File(Base):
     name = Column(String, nullable=False, unique=False)  # 파일 이름(확장자포함)
     file = Column(LargeBinary, nullable=False)  # 평준화된 오디오 파일
     url = Column(String, nullable=False, unique=True)
+    image_url = Column(String, nullable=True)
 
     __table_args__ = (
         UniqueConstraint('name', 'url'),
