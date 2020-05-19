5aa0af040c5bb8813295bddd4188ef55b2f668cf
SanghunShin <tlstkdgns95@gmail.com>
2020-05-19 23:18:32 +0900

    오타 수정및 useEffect가 props가 바뀌면 실행되게 설정

--- src/web/src/component/ViewerReact.js
+++ src/web/src/component/ViewerReact.js
@@ -18,12 +18,12 @@ const ViewerReact = (props) => {
           borderDash: [],
           borderDashOffset: 0.0,
           borderJoinStyle: "miter",
-          pointBorderColor: "rgb(205, 130,1 58)",
+          pointBorderColor: "rgb(205, 130, 158)",
           pointBackgroundColor: "rgb(255, 255, 255)",
           pointBorderWidth: 10,
           pointHoverRadius: 5,
           pointHoverBackgroundColor: "rgb(0, 0, 0)",
-          pointHoverBorderColor: "rgba(220, 220, 220,1)",
+          pointHoverBorderColor: "rgba(220, 220, 220, 1)",
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
@@ -145,7 +145,7 @@ const ViewerReact = (props) => {
     } catch (e) {
       console.log(e);
     }
-  }, []);
+  }, [props]);
   return (
     <MDBContainer>
       <h3 className="mt-5">Viewer chatting react</h3>
