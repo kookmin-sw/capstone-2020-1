faa4333fbc5b06252b225e2929624b5b7231f044
SanghunShin <tlstkdgns95@gmail.com>
2020-05-21 23:26:59 +0900

    7감정 api 와 연동 성공

--- src/web/src/component/Seven.js
+++ src/web/src/component/Seven.js
@@ -3,10 +3,10 @@ import axios from "axios";
 import { Line } from "react-chartjs-2";
 import { MDBContainer } from "mdbreact";
 
-const Seven = () => {
-  const state = {
+const Seven = (props) => {
+  let state = {
     dataLine: {
-      labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      labels: [],
       datasets: [
         {
           label: "joy",
@@ -27,7 +27,7 @@ const Seven = () => {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [3, 2, 5, 1, 6, 2, 7, 9, 25, 2],
+          data: [],
         },
         {
           label: "love",
@@ -48,7 +48,7 @@ const Seven = () => {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [2, 5, 10, 9, 12, 8, 1, 2, 10, 15],
+          data: [],
         },
         {
           label: "angry",
@@ -69,7 +69,7 @@ const Seven = () => {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [6, 9, 12, 24, 1, 0, 9, 13, 17, 22],
+          data: [],
         },
         {
           label: "neutral",
@@ -90,7 +90,7 @@ const Seven = () => {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [10, 3, 1, 22, 9, 3, 7, 22, 13, 1],
+          data: [],
         },
         {
           label: "sadness",
@@ -111,7 +111,7 @@ const Seven = () => {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [4, 8, 0, 12, 29, 15, 0, 3, 0, 24],
+          data: [],
         },
         {
           label: "fear",
@@ -132,7 +132,7 @@ const Seven = () => {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [1, 0, 0, 22, 19, 25, 10, 13, 10, 14],
+          data: [],
         },
         {
           label: "surprise",
@@ -153,16 +153,238 @@ const Seven = () => {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [25, 22, 29, 14, 0, 19, 10, 19, 20, 4],
+          data: [],
         },
       ],
     },
   };
+  const [test, setTest] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: "joy",
+        fill: true,
+        lineTension: 0.3,
+        backgroundColor: "rgba(225, 204,230, .3)",
+        borderColor: "rgb(205, 130, 158)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgb(205, 130, 158)",
+        pointBackgroundColor: "rgb(255, 255, 255)",
+        pointBorderWidth: 10,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgb(0, 0, 0)",
+        pointHoverBorderColor: "rgba(220, 220, 220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [],
+      },
+      {
+        label: "love",
+        fill: true,
+        lineTension: 0.3,
+        backgroundColor: "rgba(184, 185, 210, .3)",
+        borderColor: "rgb(35, 26, 136)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgb(35, 26, 136)",
+        pointBackgroundColor: "rgb(255, 255, 255)",
+        pointBorderWidth: 10,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgb(0, 0, 0)",
+        pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [],
+      },
+      {
+        label: "angry",
+        fill: true,
+        lineTension: 0.3,
+        backgroundColor: "rgba(184, 185, 210, .3)",
+        borderColor: "rgb(135, 26, 136)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgb(135, 26, 136)",
+        pointBackgroundColor: "rgb(255, 255, 255)",
+        pointBorderWidth: 10,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgb(0, 0, 0)",
+        pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [],
+      },
+      {
+        label: "neutral",
+        fill: true,
+        lineTension: 0.3,
+        backgroundColor: "rgba(184, 185, 210, .3)",
+        borderColor: "rgb(135, 126, 36)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgb(135, 126, 36)",
+        pointBackgroundColor: "rgb(255, 255, 255)",
+        pointBorderWidth: 10,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgb(0, 0, 0)",
+        pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [],
+      },
+      {
+        label: "sadness",
+        fill: true,
+        lineTension: 0.3,
+        backgroundColor: "rgba(184, 185, 210, .3)",
+        borderColor: "rgb(35, 76, 36)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgb(35, 76, 36)",
+        pointBackgroundColor: "rgb(255, 255, 255)",
+        pointBorderWidth: 10,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgb(0, 0, 0)",
+        pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [],
+      },
+      {
+        label: "fear",
+        fill: true,
+        lineTension: 0.3,
+        backgroundColor: "rgba(184, 185, 210, .3)",
+        borderColor: "rgb(35, 76, 116)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgb(35, 76, 116)",
+        pointBackgroundColor: "rgb(255, 255, 255)",
+        pointBorderWidth: 10,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgb(0, 0, 0)",
+        pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [],
+      },
+      {
+        label: "surprise",
+        fill: true,
+        lineTension: 0.3,
+        backgroundColor: "rgba(184, 185, 210, .3)",
+        borderColor: "rgb(135, 176, 116)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgb(135, 176, 116)",
+        pointBackgroundColor: "rgb(255, 255, 255)",
+        pointBorderWidth: 10,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgb(0, 0, 0)",
+        pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [],
+      },
+    ],
+  });
+
+  const [load, setLoad] = useState(false);
+
+  useEffect(() => {
+    try {
+      axios
+        .get("http://localhost:8000/api/predict7", {
+          headers: { "Content-Type": "multipart/form-data" },
+          params: {
+            url: props.url,
+          },
+        })
+        .then((response) => {
+          const data = response.data;
+          console.log(data.predict.pos);
+          for (var i = 0; i < 100; i++) {
+            state.dataLine.labels = state.dataLine.labels.concat(i);
+            state.dataLine.datasets[0].data = state.dataLine.datasets[0].data.concat(
+              data.predict.joy[i]
+            );
+            state.dataLine.datasets[1].data = state.dataLine.datasets[1].data.concat(
+              data.predict.love[i]
+            );
+            state.dataLine.datasets[2].data = state.dataLine.datasets[2].data.concat(
+              data.predict.anger[i]
+            );
+            state.dataLine.datasets[3].data = state.dataLine.datasets[3].data.concat(
+              data.predict.neutral[i]
+            );
+            state.dataLine.datasets[4].data = state.dataLine.datasets[4].data.concat(
+              data.predict.sadness[i]
+            );
+            state.dataLine.datasets[5].data = state.dataLine.datasets[5].data.concat(
+              data.predict.fear[i]
+            );
+            state.dataLine.datasets[6].data = state.dataLine.datasets[6].data.concat(
+              data.predict.surprise[i]
+            );
+          }
+          for (var i = 99; i > 0; i--) {
+            if (
+              state.dataLine.datasets[0].data[i] === 0 &&
+              state.dataLine.datasets[1].data[i] === 0 &&
+              state.dataLine.datasets[2].data[i] === 0 &&
+              state.dataLine.datasets[3].data[i] === 0 &&
+              state.dataLine.datasets[4].data[i] === 0 &&
+              state.dataLine.datasets[5].data[i] === 0 &&
+              state.dataLine.datasets[6].data[i] === 0
+            ) {
+              state.dataLine.labels.splice(i, 1);
+              state.dataLine.datasets[0].data.splice(i, 1);
+              state.dataLine.datasets[1].data.splice(i, 1);
+              state.dataLine.datasets[2].data.splice(i, 1);
+              state.dataLine.datasets[3].data.splice(i, 1);
+              state.dataLine.datasets[4].data.splice(i, 1);
+              state.dataLine.datasets[5].data.splice(i, 1);
+              state.dataLine.datasets[6].data.splice(i, 1);
+            } else {
+              break;
+            }
+          }
+          console.log(state.dataLine);
+          setTest(state.dataLine);
+          setLoad(true);
+        })
+        .catch();
+    } catch (e) {
+      console.log(e);
+    }
+  }, [props]);
 
   return (
     <MDBContainer>
       <h3 className="mt-5">Seven</h3>
-      <Line data={state.dataLine} options={{ responsive: true }} />
+      {load ? <Line data={test} options={{ responsive: true }} /> : <></>}
     </MDBContainer>
   );
 };
