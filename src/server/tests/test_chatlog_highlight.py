3e37b9291d43f39861366939d973ed2bc72d0499
JinseonHeo <wlstjs2478@naver.com>
2020-05-22 23:41:30 +0900

    feat: chatlog_highlight api의 test 코드 수정
    Related to: #114

--- src/server/tests/test_chatlog_highlight.py
+++ src/server/tests/test_chatlog_highlight.py
@@ -1,10 +1,8 @@
 def test_get_chatlog_highlight(client):
-    data = {
-        'videoid': '53773494'
-    }
+    data = {}
     res = client.get('api/chatlog_highlight', query_string=data)
     assert res.status_code == 400
 
-    data['platform'] = 'AfreecaTV'
+    data = {"url": "http://vod.afreecatv.com/PLAYER/STATION/96825198"} # 유효하지 않은 url
     res = client.get('api/chatlog_highlight', query_string=data)
-    assert res.status_code == 200
+    assert res.status_code == 406
